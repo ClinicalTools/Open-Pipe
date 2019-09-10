753c69515ceaa49af3a63b6c6f5a2ca7a09aa778
CTVilgalys <vilgalys@clinicaltools.com>
2019-09-10 15:11:13 -0400

    First batch of assets

---
+++ Assets/Editor/DialogueTriggerEditor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(DialogueTrigger))]
+public class DialogueTriggerEditor : Editor
+{
+
+}
---
+++ Assets/Editor/TextAreaEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Interactions)), CanEditMultipleObjects]
+public class TextAreaEditor : Editor
+{
+    //Get the serialized property of the options 
+    public SerializedProperty optionsString;
+    /// <summary>
+    /// Find all instances of options
+    /// </summary>
+    void OnEnable()
+    {
+        optionsString = serializedObject.FindProperty("options");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        //Update the inspector GUI so that everything is editable.
+        serializedObject.Update();
+        optionsString.stringValue = EditorGUILayout.TextArea(optionsString.stringValue, GUILayout.MaxHeight(75));
+        serializedObject.ApplyModifiedProperties();
+
+
+    }
+}
